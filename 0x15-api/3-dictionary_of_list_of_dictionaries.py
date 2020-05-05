4e5881f03967eb29209bb6b7500b999be7fc3378
FaroukAjimi <benajimi@gmail.com>
2020-05-05 03:42:31 +0000

    Zconfition

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,26 +4,25 @@ import requests
 import sys
 from collections import OrderedDict
 if __name__ == '__main__':
-    if len(sys.argv) == 2 and sys.argv[1].isdigit():
-        dta = OrderedDict()
-        res1 = requests.get('https://jsonplaceholder.typicode.com/todos')
-        res2 = requests.get('https://jsonplaceholder.typicode.com/users')
-        s = res1.json()
-        users = res2.json()
-        for i in range(1, len(users) + 1):
-            info = request.get(
-                'https://jsonplaceholder.typicode.com/todos?userId={}'
-                .format(i))
-            usern = request.get(
-                'https://jsonplaceholder.typicode.com/users/{}'.format(i))
-            username = usern.json().get('username')
-            to = info.json()
-            dta[str(i)] = []
-            for d in to:
-                ele = OrderedDict()
-                ele['username'] = username
-                ele['task'] = d.get('title')
-                ele['completed'] = d.get('completed')
-                data[str(i)].append(ele)
-        with open('todo_all_employees.json', 'w+', newline='') as f:
-            json.dump(dta, f)
+    dta = OrderedDict()
+    res1 = requests.get('https://jsonplaceholder.typicode.com/todos')
+    res2 = requests.get('https://jsonplaceholder.typicode.com/users')
+    s = res1.json()
+    users = res2.json()
+    for i in range(1, len(users) + 1):
+        info = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'
+            .format(i))
+        usern = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(i))
+        username = usern.json().get('username')
+        to = info.json()
+        dta[str(i)] = []
+        for d in to:
+            ele = OrderedDict()
+            ele['username'] = username
+            ele['task'] = d.get('title')
+            ele['completed'] = d.get('completed')
+            dta[str(i)].append(ele)
+    with open('todo_all_employees.json', 'w+', newline='') as f:
+        json.dump(dta, f)
