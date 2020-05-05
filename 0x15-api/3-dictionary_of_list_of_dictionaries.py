9a6f6c235aa5d7b3263f4d513779870ea4feea51
FaroukAjimi <benajimi@gmail.com>
2020-05-05 03:12:28 +0000

    Di

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,44 +4,22 @@ import requests
 import sys
 if __name__ == '__main__':
     if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        dta = OrderedDict()
         res1 = requests.get('https://jsonplaceholder.typicode.com/todos')
         res2 = requests.get('https://jsonplaceholder.typicode.com/users')
         s = res1.json()
-        users = []
-        usr = res2.json()
-        ict = []
-        for y in usr:
-                users.append(y['username'])
-        for i in s:
-                ict.append(i['userId'])
-        length = ict.count(1)
-        first = False
-        count = 0
-        with open('to_do_all_employees.json', 'w+') as f:
-                f.write('{')
-                for u in users:
-                        count = 0
-                        first = False
-                        for i in s:
-                                try:
-                                    for key, value in i.items():
-                                        fo = users.idex(u) + 1
-                                        if key == 'userId' and value == fo:
-                                            if first is False:
-                                                f.write('"{}": ['
-                                                        .format(i['userId']))
-                                                first = True
-                                                f.write(
-                                                    json.dumps(
-                                                        {'task': i['title'],
-                                                         'completed':
-                                                         i['completed'],
-                                                         'username': u}))
-                                                count += 1
-                                                if count != length:
-                                                    f.write(", ")
-                                except:
-                                    pass
-                        f.write(']}')
-                        if u != users[-1]:
-                            f.write(', ')
+        users = res2.json()
+        for i in range(1, len(users) + 1):
+            info = request.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(i))
+            usern = request.get('https://jsonplaceholder.typicode.com/users/{}'.format(i))
+            username = usern.json().get('username')
+            to = info.json()
+            dta[str(i)] = []
+            for d in to:
+                ele = OrderedDict()
+                ele['username'] = username
+                ele['task'] = d.get('title')
+                ele['completed'] = d.get('completed')
+                data[str(i)].append(ele)
+        with open('todo_all_emplyees.json', 'w+', newline='') as f:
+            json.dump(dta, f)
