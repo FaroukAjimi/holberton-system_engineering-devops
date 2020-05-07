0b5288f47b937fc79adf2e8bc15ef939581261ee
FaroukAjimi <benajimi@gmail.com>
2020-05-07 22:08:57 +0000

    Goo

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-top ten comments
+recursion comments
 """
 
 import json
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], next=""):
     """function that search for the best first
     ten hot comments"""
     headers = {
@@ -18,15 +18,29 @@ def recurse(subreddit, hot_list=[]):
                           .format(subreddit),
                           headers=headers)
     jsonred = reddit.json()
-    i = 0
     try:
-        while (jsonred['data']['children'][i])['data']['title']:
-            s = (jsonred['data']['children'][i])['data']['title']
-            if s == "this sub doesn't exist":
-                raise Exception
-            hot_list.append(s)
-            i = i + 1
+        hot_list.extend(jsonred['data']['children'])
+        next = jsonred['data']['after']
+        if jsonred['data']['after'] is None:
+            return
     except:
         return(None)
-    finally:
-        return(hot_list)
+    getfoward(subreddit, hot_list, next)
+    return(hot_list)
+
+
+def getfoward(subreddit, hot_list=[], next=""):
+    """getting all next pages"""
+    if next is None:
+        return
+    headers = {
+        'User-Agent': 'My User Agent 1.0',
+        'From': '1044@holbertonschool.com'
+    }
+    reddit = requests.get("https://www.reddit.com/r/{}/hot.json"
+                          .format(subreddit),
+                          headers=headers,
+                          params={'after': next, 'limit': 100})
+    jsonred = reddit.json()
+    hot_list.extend(jsonred['data']['children'])
+    return getfoward(subreddit, hot_list, jsonred['data']['after'])
