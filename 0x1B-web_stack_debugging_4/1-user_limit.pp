98d9db274ab0d6ef13cdcb533f828ecde451d197
FaroukAjimi <benajimi@gmail.com>
2020-05-29 12:48:49 +0100

    task1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+exec {'fix soft':
+path    => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
+command => "sudo sed -i 's/holberton soft nofile 4/holerton soft nofile 700000/' /etc/security/limits.conf; /sbin/sysctl -p",
+}
+exec {'fix hard':
+path    => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
+command => "sudo sed -i 's/holberton hard nofile 5/holerton hard nofile 700000/' /etc/security/limits.conf; /sbin/sysctl -p",
+}
